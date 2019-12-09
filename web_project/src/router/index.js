cd9d483af16d32169d189fdebe2379a0bf1d4e9e
gmldbs <gmldbs3445@gmail.com>
2019-12-09 14:07:19 +0900

    peristalsis vue and firebase and add g-maps modules

--- web_project/src/router/index.js
+++ web_project/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const routerOptions = [
+  { path: '/', component: 'HelloWorld' }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = new Router({
+  mode: 'history',
+  routes
 })
+export default router
