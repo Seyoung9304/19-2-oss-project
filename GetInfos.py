512d1abe04a738d040130ef90fbc2a0d5d5ad71c
SeyoungKim <50132040+Seyoung9304@users.noreply.github.com>
2019-12-19 03:09:02 +0900

    [Update] google 별점 받아오고 firestore에 올리기

--- GetInfos.py
+++ GetInfos.py
@@ -8,6 +8,7 @@ import urllib
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import firestore
 from urllib.parse import quote_plus
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -22,11 +23,17 @@ def print_restaurant_name_google(name):
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     try:
-        r = soup.select('.Ob2kfd')
+        r = soup.select('.Ob2kfd') #정보가 우측에 단독으로 나오는 경우
+        l = soup.select('.rllt__details.lqhpac') #정보가 리스트로 나오는 경우
         # print(type(r))
         for i in r:
             print("Google score: " + i.select_one('.Aq14fc').text)
-        driver.close()
+            driver.close()
+            return i.select_one('.Aq14fc').text
+        for j in l:
+            print("Google score: " + j.select_one('.BTtC6e').text)
+            driver.close()
+            return j.select_one('.BTtC6e').text
     except:
         print("No data or error!")
 
@@ -44,15 +51,18 @@ def print_restaurant_name_siksin(name):
     except:
         print("No data or error!")
 
-# firebase 인증 및 초기화
-cred = credentials.Certificate('mykey.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://oss-d85b6.firebaseio.com/'
-})
 
-ref = db.reference()
-siname = ["가평군", "고양시", "과천시", "광주시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시", "안성시", "안양시", "양주시", "여주시", "연천군", "오산시", "용인시", "의왕시", "의정부시", "이천시", "평택시", "파주시", "포천시", "하남시", "화성시"]
+# firebase 인증 및 초기화
+cred = credentials.Certificate('./mykey.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection(u'data')
+# ref = db.reference()
+siname = ["가평군", "고양시", "과천시", "광주시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시", "안성시", "안양시", "양주시",
+          "여주시", "연천군", "오산시", "용인시", "의왕시", "의정부시", "이천시", "평택시", "파주시", "포천시", "하남시", "화성시"]
 for names in siname:
+
     name = urllib.parse.quote(names)
     url = 'https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt?KEY=30c8bab88c6249babce184a75ce9be0f&Type=json&SIGUN_NM=' + name
     request = urllib.request.Request(url)
@@ -69,18 +79,24 @@ for names in siname:
                 data.append([i['RESTRT_NM'], i['TASTFDPLC_TELNO'], i['REFINE_ROADNM_ADDR'], i['REFINE_WGS84_LAT'],
                              i['REFINE_WGS84_LOGT']])
                 storename.append(i['RESTRT_NM'])
-                ref = db.reference(name+'/'+i['RESTRT_NM'])
-                ref.update({'storename': i['RESTRT_NM']})
-                ref.update({'telno': i['TASTFDPLC_TELNO']})
-                ref.update({'address': i['REFINE_ROADNM_ADDR']})
-                ref.update({'longitude': i['REFINE_WGS84_LOGT']})
-                ref.update({'latitude': i['REFINE_WGS84_LAT']})
-                #맛집 기본정보 업데이트
+                star1 = print_restaurant_name_google(i['RESTRT_NM'])
+                doc_ref.document(i['RESTRT_NM']).set({
+                    u'si': names,
+                    u'score': star1,
+                    u'storename': i['RESTRT_NM'],
+                    u'telno': i['TASTFDPLC_TELNO'],
+                    u'address': i['REFINE_ROADNM_ADDR'],
+                    u'longitude': i['REFINE_WGS84_LOGT'],
+                    u'latitude': i['REFINE_WGS84_LAT']
+                })
+                # 맛집 기본정보 업데이트
+            """
             for i in storename:
                 print("---------------------------------")
                 print("Search for " + i + ":")
-                #print_restaurant_name_siksin(i)
-                #print_restaurant_name_google(i)
+                # print_restaurant_name_siksin(i)
+                # print_restaurant_name_google(i)
+            """
         except:
             print(dict['RESULT']['MESSAGE'])
     else:
