bb937bbe7ece37a3a25e616beeec14e19b567041
gmldbs <gmldbs3445@gmail.com>
2019-12-19 03:02:06 +0900

    웹에다가 데이터 가져와서 핀띄우기

--- web_project/src/main.js
+++ web_project/src/main.js
@@ -10,7 +10,8 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBWfBpAVmaw98fBmVWRVzdNumZXrEZ0qNM'
+    key: 'AIzaSyBWfBpAVmaw98fBmVWRVzdNumZXrEZ0qNM',
+    libraries: 'places'
   }
 })
 
